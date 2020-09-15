b0451e16b6775d6d5055d733b8cdaea015dd8020
Alex <tektonikboy98@gmail.com>
2020-09-15 20:16:24 +0600

    Fixed password reset

--- users/models.py
+++ users/models.py
@@ -148,7 +148,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.username,
         'email': reset_password_token.user.email,
-        'reset_password_url': "?token={}".format(reset_password_token.key)
+        'reset_password_url': "https://vrmates.co/change-password/?token={token}".format(token=reset_password_token.key)
     }
 
     # render email text
