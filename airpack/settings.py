ef731a9b58f1d44be3286fe670b89e7b9679c227
Alex <tektonikboy98@gmail.com>
2020-09-16 10:02:35 +0600

    Static files fix

--- airpack/settings.py
+++ airpack/settings.py
@@ -174,10 +174,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Heroku settings
